08f60f7f2afd70e6b0175e7ad6cf5bac7dc19811
Kevin Song <kevin@Kevins-MacBook-Air.local>
2013-06-16 07:25:52 -0700

    Checkout Button Works. PayPal next.

--- Checkout/PayPalViewController.m
+++ Checkout/PayPalViewController.m
@@ -35,7 +35,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)pay{
+- (IBAction)payBtn{
  
     //Create a PayPal Payment
     PayPalPayment *payment = [[PayPalPayment alloc] init];
